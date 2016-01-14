bc9247635119064cb7e79c140eab55af15a4a4b6
= <=>
2016-01-14 22:13:13 +0100

    primer

--- bin/mem.py
+++ bin/mem.py
@@ -1,3 +1,14 @@
-import time
-s = bytearray(1024000000) #1000 MB
-time.sleep(900) #Keep the RAM as long as possible
+import urllib.request
+#baseurl = "http://superunicorn.mybluemix.net"
+baseurl = "http://127.0.0.1:8080"
+
+for j in range(0,100): #100 prime numbers
+	p = int(urllib.request.urlopen(baseurl+"/getjob").read())
+	print("Checking number %s" % p)
+
+	for i in range(2,p):
+		if (p % i) == 0:
+			urllib.request.urlopen(baseurl+"/noprime?number=%s" % p)
+			break
+	else:
+		urllib.request.urlopen(baseurl+"/prime?number=%s" % p)
