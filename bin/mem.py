1caf079832ba10eddfa3ac611d53aa1807101cf3
= <=>
2016-01-18 15:30:30 +0100

    No hash, random

--- bin/mem.py
+++ bin/mem.py
@@ -1,8 +1,8 @@
-import urllib.request, time, hashlib
+import urllib.request, time, random, string
 baseurl = "http://superunicorn.mybluemix.net"
 #baseurl = "http://127.0.0.1:8080"
 
-appID = hashlib.sha224(str(time.time()).hexdigest()[-6:])
+appID = ''.join(random.choice(string.ascii_uppercase) for _ in range(8))
 
 i = 0
 while i < 10:
