a5786adf0f161b58e229d4cb66d441887c475d27
= <=>
2016-01-18 10:38:40 +0100

    send pings

--- bin/mem.py
+++ bin/mem.py
@@ -1,14 +1,7 @@
-import urllib.request
+import urllib.request, time
 baseurl = "http://superunicorn.mybluemix.net"
 #baseurl = "http://127.0.0.1:8080"
 
-for j in range(0,100): #100 prime numbers
-	p = int(urllib.request.urlopen(baseurl+"/getjob").read())
-	print("Checking number %s" % p)
-
-	for i in range(2,p):
-		if (p % i) == 0:
-			urllib.request.urlopen(baseurl+"/noprime?number=%s" % p)
-			break
-	else:
-		urllib.request.urlopen(baseurl+"/prime?number=%s" % p)
+while True:
+	urllib.request.urlopen(baseurl+"/alive")
+	time.sleep(5)
