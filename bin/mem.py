8942748bfbb986df754a80f18ffcc49b121f4997
= <=>
2016-01-18 11:42:06 +0100

    send pings

--- bin/mem.py
+++ bin/mem.py
@@ -2,6 +2,8 @@ import urllib.request, time
 baseurl = "http://superunicorn.mybluemix.net"
 #baseurl = "http://127.0.0.1:8080"
 
+appID = str(time.time())
+
 while True:
-	urllib.request.urlopen(baseurl+"/alive")
+	urllib.request.urlopen(baseurl+"/alive?appID=" + appID)
 	time.sleep(5)
