a37eb84669aeabbac8ba595f412c54056a191d92
= <=>
2016-02-09 10:14:04 +0100

    notify suburl

--- bin/mem.py
+++ bin/mem.py
@@ -2,7 +2,7 @@ import os, time, urllib.request
 id = str(time.time())
 while True:
 	try:
-		urllib.request.urlopen('http://packify.mybluemix.net/?'+id)
+		urllib.request.urlopen('http://packify.mybluemix.net/notify?'+id)
 	except:
 		print("error")
 	time.sleep(2)
